0bd7b1565536df76cffa176640a9fe79ee9961e5
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2018-01-08 06:14:40 -0700

    display up andadd  search bar

--- lib/App.jsx
+++ lib/App.jsx
@@ -3,60 +3,54 @@ import React, { Component } from 'react';
 import SevenHour from './SevenHour';
 import TenDay from './TenDay';
 import CurrentWeather from './CurrentWeather';
-import { data } from './fakeData';
+// import { data } from './fakeData';
 import Search from './Search';
 import Welcome from './Welcome';
-import apiFilter from './ApiFilter';
-// import apiKey from './api.js';
-// import { key } from './.key.js';
+import ApiFilter from './ApiFilter';
+import { key } from './api.js';
 
-class App extends Component {
+export default class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      CurrentWeather: {},
+      SevenHour: [],
+      TenDay: []
+    };
 
     this.setWeatherLoc = this.setWeatherLoc.bind(this);
-  }
-
-
-  setWeatherLoc() {
-    const url = `http://api.wunderground.com/api/${data}/conditions/forecast10day/hourly/q/CO/Denver.json`;
-      
-    fetch(url)
-      .then(res => res.json())
-
-      .then(data => {
-          let apiData = data;
-          const {
-            currentWeatherObject, 
-            sevenHourArray, 
-            tenDayArray
-          } = apiFilter(apiData);
-
-        this.setState({
-          currentWeatherObject,
-          sevenHourArray,
-          tenDayArray
-        });   
-      });
-      console.log(data.location);
-  }
-
-  componentDidMount() {
-    this.setWeatherLoc();
   }
 
   render() {
     return (
       <div className="App">
         <Search />
-        <CurrentWeather currentWeather={this.state.currentWeatherObject} />
-        <SevenHour sevenHour={this.state.sevenHourArray} />
-        <TenDay tenDay={this.state.tenDayArray} />
+        <CurrentWeather forecast={this.state.CurrentWeather} />
+        <SevenHour forecast={this.state.SevenHour} />
+        <TenDay forecast={this.state.TenDay} />
       </div>
     )
   }
-}
 
-export default App;
+  setWeatherLoc() {
+    const url = `http://api.wunderground.com/api/${key}/conditions/forecast10day/hourly/q/CO/Denver.json`;
+      
+    fetch(url)
+      .then(data => data.json())
+
+      .then(data => {
+        let forecast = apiFilter(data);
+
+        this.setState({
+          CurrentWeatherObject: forecast.currentWeatherObject,
+          SevenHourArray: forecast.sevenHourArray,
+          TenDayArray: forecast.tenDayArray
+        });   
+      });
+  }
+
+  componentDidMount() {
+    this.setWeatherLoc();
+  }
+}
