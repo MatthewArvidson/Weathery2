a23175a20e2c6a3210b4a5fcc1f9a68e328dbc55
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2018-01-08 21:27:33 -0700

    Import prefixTrie from my completeMe

--- lib/Search.jsx
+++ lib/Search.jsx
@@ -1,14 +1,19 @@
 import '../styles/Search.css';
 import '../styles/normalize.css';
 import React from 'react';
+import data from './cities';
+import { Trie } from  '@MatthewArvidson/completeMe';
 
 export default class Search extends React.Component {
   constructor() {
 
     super();
+    this.trie = new Trie();
+    this.trie.populate(data.data);
 
     this.state = {
-      location: ''
+      input: '',
+      suggestedCity: []
     };
   }
 
